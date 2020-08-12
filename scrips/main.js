92d15f173975e5be557a128eca05544ba62025fa
JulSarmiento <jasa1999@hotmail.com>
2020-08-12 12:24:06 -0500

    proyecto terminado: HTML, CSS y JS

---
+++ scrips/main.js
@@ -0,0 +1,104 @@
+const results = document.querySelector('.cards-container');
+const form = document.querySelector("#inscription");
+const filters = document.querySelectorAll('.filter');
+let allDogs = [];
+
+/**
+ * Añade un perrito y lo guarda en el localstorange convertido a JSON.
+ */
+function addDog(name, age, sex, breed, specialCondition, size, behavior, status) {
+    let newDog = new Dog(name, age, sex, breed, specialCondition, size, behavior, status);
+    allDogs.push(newDog)
+    results.insertAdjacentHTML('beforeend', printDogCard(newDog));
+
+    localStorage.setItem('Cards', JSON.stringify(allDogs));
+}
+
+/**
+ * Esta funcion filtra los perros segundo el parametro del status (adoptado, en adopción y en proceso de adopción.)
+ */
+function filterDogsstupidHabisFunction(status) {
+    return allDogs.filter((dog) => {
+        return dog.status === status 
+    });
+}
+
+/**
+ * Este foEach acomoda el parametro para cada evento de botón segund su ID (status)
+ */
+filters.forEach((filter) => {
+    filter.addEventListener('click', (event) => {
+        const iD = event.target.id;
+        let stupidHabisFilter;
+        if(iD === 'all-dogs'){
+            stupidHabisFilter = allDogs;
+        }else if(iD === 'in-adoption') {
+            stupidHabisFilter = filterDogsstupidHabisFunction('En adopción');
+        }else if(iD === 'in-process'){
+            stupidHabisFilter = filterDogsstupidHabisFunction('En proceso de adopción');
+        }else {
+            stupidHabisFilter = filterDogsstupidHabisFunction('Adoptado');
+        }
+        results.innerHTML = '';
+        stupidHabisFilter.forEach((dog) => {
+            results.insertAdjacentHTML('beforeend', printDogCard(dog))
+        })
+        console.log(stupidHabisFilter)
+        console.log(`Le di click a ${iD}`)
+    })
+   
+})
+
+/**
+ *Esta funcion imprime la carda del perro con sus respectivas propiedades. 
+    */
+function printDogCard(dog) {
+    return `        
+        <div class="card">
+            <img class="card-picture" src="assets/images/perro1.jpeg" alt="foto del perro 1">
+            <aside class="card-information">
+                <h3 class="card-information-name">Nombre: <span> ${dog.name}</span> </h3>
+                <p class="card-information-text">Edad: <span>${dog.age}</span> </p>
+                <p class="card-information-text">Sexo:  <span>${dog.sex}</span> </p>
+                <p class="card-information-text">Raza: <span>${dog.breed}</span>  </p>
+                <p class="card-information-text">Condiciones especiales: <span>${dog.specialCondition}</span>  </p>
+                <p class="card-information-text">Tamaño:  <span>${dog.size}</span></p>
+                <p class="card-information-text">Temperamento:  <span>${dog.behavior}</span> </p>
+                <p class="card-information-text">Estado: <span>${dog.status}</span></p>
+            </aside>
+        </div>`
+}
+
+/**
+ * Este es el evento del boton submit del formulario y se organizan los datos recibidos de cada casilla en su respectiva varible acorde a las propiedades del obejto perrito.
+ */
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const data = new FormData(form);
+    const name = data.get('name');
+    const age = data.get('age');
+    const sex = data.get('sex');
+    const breed = data.get('breed');
+    const specialCondition =  data.get('special');
+    const size = data.get('size');
+    const behavior = data.get('personality');
+    const status = data.get('status'); 
+
+    addDog(name, age, sex, breed, specialCondition, size, behavior, status);
+})
+
+/**
+ * Este evento carga el localstorage cuando la pantalla vuelve a cargar para mostrar las cards guardadas de los perritos.
+ */
+window.addEventListener('load', () => {
+    const dataStored = localStorage.getItem('Cards');
+    
+    if(dataStored) {
+        allDogs = JSON.parse(dataStored);
+        allDogs.forEach((dog) => {
+            results.insertAdjacentHTML('beforeend', printDogCard(dog))
+        })
+        
+    }
+   //results.innerHTML = localStorage.getItem('Cards');
+})
